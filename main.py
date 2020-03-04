f0f188c2864aac2ac68a821a73a02c0882fbf997
jimth001 <rmwangyl@qq.com>
2020-03-04 10:54:32 +0800

    Version 1.1.2: test training and inferring pipeline of version 1.1.x

--- main.py
+++ main.py
@@ -7,15 +7,28 @@ from TextPreprocessing.gpt_bpe_tool import get_encoder, Encoder
 
 
 def train():
+    data_path = './data/my_s2s_test_data/train2.tsv'
     model = GPTModel()
+    model.check_after_init()
+    text2index_dictionary_path = './data/bpe_codes/'
+    bpe_tool = get_encoder(text2index_dictionary_path)
+    train_stream = DataStream(data_path, placeholder_meta_data=model.placeholders_meta_data,
+                              func_for_task_specific_preprocessing=model.process_origin_data_for_placeholders,
+                              text_preprocessor=None,
+                              text2index_tool=bpe_tool,
+                              shuffle_each_epoch=True, round_feeding=True, in_tsv_mode=True)
+    dev_stream = DataStream(data_path, placeholder_meta_data=model.placeholders_meta_data,
+                            func_for_task_specific_preprocessing=model.process_origin_data_for_placeholders,
+                            text_preprocessor=None,
+                            text2index_tool=bpe_tool,
+                            shuffle_each_epoch=False, round_feeding=False, in_tsv_mode=True)
     trainer = ModelEstimator(device_id=[0], model_fn=model)
-    trainer.training(train_data_path='./data/my_s2s_test_data/train.tsv',
-                     dev_data_path='./data/my_s2s_test_data/dev.tsv',
-                     ckpt_dir='./data/my_s2s_models_adam/',
-                     learning_rate=1e-4, batch_size=64, mini_batch=8,
-                     total_steps=5000, eval_per_n_steps=100, max_to_save=1,
+    trainer.training(train_stream, dev_stream,
+                     ckpt_dir='./data/my_s2s_models_test/',
+                     learning_rate=1e-4, batch_size=16, mini_batch=8,
+                     total_steps=100, eval_per_n_steps=1, max_to_save=1,
                      early_stop_steps=500,
-                     pretrained_ckpt='./data/gpt2_pre_trained_model')
+                     pretrained_ckpt='./data/gpt2_pre_trained_model/')
 
 
 def test():
@@ -23,19 +36,40 @@ def test():
     model.config['decode_length'] = 60
     model.config['beam_size'] = 8
     model.config['decode_alpha'] = 0.6
-    data_stream = DataStream('./data/my_s2s_test_data/test2.tsv', placeholder_meta_data=model.config['placeholders'],
+    text2index_dictionary_path = './data/bpe_codes/'
+    bpe_tool = get_encoder(text2index_dictionary_path)
+    data_stream = DataStream('./data/my_s2s_test_data/test2.tsv', placeholder_meta_data=model.placeholders_meta_data,
                              func_for_task_specific_preprocessing=model.process_origin_data_for_placeholders,
+                             text_preprocessor=None,
+                             text2index_tool=bpe_tool,
                              shuffle_each_epoch=False, round_feeding=False, in_tsv_mode=True)
     model.config['eos_id'] = data_stream.text_index_encoder.eos_id
+    model.check_after_init()
     my_estimator = ModelEstimator(device_id=[0], model_fn=model)
     result = my_estimator.inferring(data_stream=data_stream,
                                     ckpt_dir='./data/my_s2s_models_adam/',
-                                    mini_batch=8, logging=True)
-    result = result['beam_search_seqs']
+                                    mini_batch=8, logging=False)
+    result = result['pred_seq']
     for one in result:
         print(data_stream.text_index_encoder.decode(
             model.parse_out_idx(one[0], eos_id=data_stream.text_index_encoder.eos_id)))
 
 
-test()
-print('test finished')
+def test_build_training_inferring_graph_simultaneously():
+    model = GPTModel()
+    model.config['decode_length'] = 60
+    model.config['beam_size'] = 8
+    model.config['decode_alpha'] = 0.6
+    model.config['eos_id'] = 50256
+    my_estimator = ModelEstimator(device_id=[0], model_fn=model)
+    model.set_vocab_size(50257)
+    my_estimator.build_data_parallel_training_graph(allow_gradient_accumulation=True)
+    my_estimator.build_data_parallel_inferring_graph()
+    my_estimator.create_session_init_and_print_all_vars(max_to_save=1,
+                                                        pretrained_ckpt='./data/gpt2_pre_trained_model/',
+                                                        logging=False)
+
+
+if __name__ == '__main__':
+    train()
+    print('test finished')
